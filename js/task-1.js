c6b922f708d47faf3008432a9c350196c0af898f
Tretiak-Anna <tretiakann@gmail.com>
2024-03-17 16:23:09 +0200

    before check

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-const numCategories = document.querySelectorAll('.item');
-console.log(`Number of Categories: ${numCategories.length}`);
-
-numCategories.forEach(item => {
-  console.log(`Category: ${item.firstElementChild.textContent}`);
-  console.log(`Elements: ${item.lastElementChild.children.length}`);
-});
