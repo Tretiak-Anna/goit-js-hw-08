0407107a2a6e452f4a63c7b3b1b9815e788d6e07
Tretiak-Anna <151523272+Tretiak-Anna@users.noreply.github.com>
2024-03-17 15:32:21 +0200

    Initial commit

---
+++ js/task-6.js
@@ -0,0 +1,30 @@
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const input = document.querySelector('input[type="number"]');
+const boxesDiv = document.getElementById('boxes');
+
+createButton.addEventListener('click', () => {
+  const inputValue = parseInt(input.value);
+  if (inputValue >= 1 && inputValue <= 100) {
+    boxesDiv.innerHTML = '';
+    let size = 30;
+    for (let i = 0; i < inputValue; i++) {
+      const box = document.createElement('div');
+      box.style.height = size + 'px';
+      box.style.width = size + 'px';
+      box.style.backgroundColor = getRandomHexColor();
+      boxesDiv.appendChild(box);
+      size += 10;
+    }
+  }
+  input.value = '';
+});
+
+destroyButton.addEventListener('click', () => {
+  boxesDiv.innerHTML = '';
+});
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
